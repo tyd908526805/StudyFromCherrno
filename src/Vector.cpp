690d93d7c98b7180f9be51ca626fec1cb8362891
VendettistM <tyd951115@gmail.com>
2023-02-06 00:19:30 -0800

    动态数组vector优化相关

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -5,6 +5,17 @@
 struct Vertex
 {
     float x, y, z;
+
+    Vertex(float x, float y, float z)
+        : x(x), y(y), z(z)
+    {
+    }
+
+    Vertex(const Vertex& vertex)
+        : x(vertex.x), y(vertex.y), z(vertex.z)
+    {
+        std::cout <<  "Copied!" << std::endl;
+    }
 };
 
 std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
@@ -21,8 +32,12 @@ int main()
 {
     // Vertex* vertices = new Vertex[500000];
     std::vector<Vertex> vertices;
-    vertices.push_back({1, 2, 3});
-    vertices.push_back({4, 5, 6});
+    vertices.reserve(3);
+    // 使用以下参数构建了Vertex对象
+    vertices.emplace_back(1, 2, 3);
+    // vertices.push_back({1, 2, 3});
+    // vertices.push_back({4, 5, 6});
+    // vertices.push_back(Vertex(4, 5, 6));
     Function(vertices);
     for (int i = 0; i < vertices.size(); i++)
     {
